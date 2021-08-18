f9f808f31df871cbc08e6b4aff8be39aef581f6f
arafat356 <ahmedarafatft@gmail.com>
2021-08-19 02:45:30 +0600

    first

---
+++ js-assignment.js
@@ -0,0 +1,46 @@
+//           problem 1:
+// ==================================          
+
+function seerToMon(seer1,seer2){
+    const sum =seer1+seer2;
+    return sum;
+}
+ 
+const firstTotal =seerToMon(64,98);
+console.log(firstTotal);
+const secondTotal=seerToMon(54,908);
+console.log(secondTotal);
+
+//         problem 2
+// ===============================   
+
+
+function totalSales(shirtQuantity,pantQuantity,shoeQuentity){
+    console.log(shirtQuantity,pantQuantity,shoeQuentity);
+    const firstTotal = shoeQuentity+pantQuantity+shoeQuentity;
+    return total;
+}
+
+const shirtQuantity=100;
+const pantQuantity=200;
+const shoeQuentity=300;
+
+const resutl =totalSales(shoeQuentity,pantQuantity,shoeQuentity);
+console.log('result value',resutl);
+
+//             problem 4
+// ====================================
+
+var perfectFriend =['asif','mubarak','mamun','aman','bablu','saifu','piyaro'];
+
+var asifIndex = perfectFriend.indexOf('asif');
+console.log(asifIndex);
+
+
+
+
+
+
+
+
+
